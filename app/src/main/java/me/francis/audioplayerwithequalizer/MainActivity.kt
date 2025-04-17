a3f500e0432b919a8f43af564b3fb07ce4a97d17
Luiz Augusto <luiz.kirk4@gmail.com>
2025-04-16 22:57:26 -0300

    Consumindo AudioService na ViewModel do player.

--- app/src/main/java/me/francis/audioplayerwithequalizer/MainActivity.kt
+++ app/src/main/java/me/francis/audioplayerwithequalizer/MainActivity.kt
@@ -1,5 +1,6 @@
 package me.francis.audioplayerwithequalizer
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,10 +10,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import me.francis.audioplayerwithequalizer.navigation.NavManager
+import me.francis.audioplayerwithequalizer.services.AudioService
 import me.francis.audioplayerwithequalizer.ui.theme.AudioPlayerWithEqualizerTheme
 import me.francis.audioplayerwithequalizer.viewModels.PlayerViewModel
 
 class MainActivity : ComponentActivity() {
+    private val playerViewModel = PlayerViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,12 +28,18 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     // view model
-                    val playerViewModel = PlayerViewModel()
 
                     // navigation
                     NavManager(playerViewModel = playerViewModel)
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart")
+        val intent = Intent(this, AudioService::class.java)
+        playerViewModel.startAudioService(intent, 0, 0) // ver quais flags e id passar
     }
 }
--- app/src/main/java/me/francis/audioplayerwithequalizer/viewModels/PlayerViewModel.kt
+++ app/src/main/java/me/francis/audioplayerwithequalizer/viewModels/PlayerViewModel.kt
@@ -1,32 +1,43 @@
 package me.francis.audioplayerwithequalizer.viewModels
 
+import android.content.Intent
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import me.francis.audioplayerwithequalizer.services.AudioService
+import me.francis.playbackmodule.PlaybackEvent
 
 class PlayerViewModel : ViewModel() {
 
     private val audioService = AudioService()
 
     // todo: reaproveitar para estados da aplicação (play/pause, etc)
-//    private val _uiUserListState = MutableStateFlow(UserListUiState())
-//    val uiListViewState = _uiUserListState.asStateFlow()
+    private val _currentPosition = MutableStateFlow<Int>(0)
+    val currentPosition = _currentPosition.asStateFlow()
 
-//    private val _uiUserDetailViewState = MutableStateFlow(UserDetailUiState())
-//    val uiUserDetailViewState = _uiUserDetailViewState.asStateFlow()
+    private val _duration = MutableStateFlow<Int>(0)
+    val duration = _duration.asStateFlow()
 
-//    private val _uiEqualizerViewState = MutableStateFlow(EqualizerUiState())
-//    val uiEqualizerViewState = _uiEqualizerViewState.asStateFlow()
+    private val _isPlaying = MutableStateFlow<Boolean>(false)
+    val isPlaying = _isPlaying.asStateFlow()
 
-//    init {
-//        viewModelScope.launch {
-//            userDao.getUsers().collectLatest {
-//                println("********** state $it")
-//                _uiUserListState.update { currenState ->
-//                    currenState.copy(userList = it)
-//                }
-//            }
-//        }
-//    }
+    private val _currentTrack = MutableStateFlow<String?>(null)
+    val currentTrack = _currentTrack.asStateFlow()
+
+    private val _playbackEvents = MutableStateFlow<PlaybackEvent>(PlaybackEvent.PlaybackStopped)
+    val playbackEvents = _playbackEvents.asStateFlow()
+
+    init {
+        audioService.onCreate()
+    }
+
+    fun startAudioService(intent: Intent, flags: Int, startId: Int) {
+        audioService.onStartCommand(
+            intent = intent,
+            flags = flags,
+            startId = startId
+        )
+    }
 
     fun playPause() {
         if (audioService.isPlaying.value) {
@@ -36,11 +47,27 @@ class PlayerViewModel : ViewModel() {
         }
     }
 
-    fun previousTrack(path: String) {
+    fun stop() {
+        audioService.stop()
+    }
+
+    fun seekTo(positionMs: Int) {
+        audioService.seekTo(positionMs = positionMs)
+    }
+
+    fun setVolume(volume: Float) {
+        audioService.setVolume(volume = volume)
+    }
+
+    fun skipToNext(path: String) {
+        audioService.skipToNext(path = path)
+    }
+
+    fun skipToPrevious(path: String) {
         audioService.skipToPrevious(path = path)
     }
 
-    fun nextTrack(path: String) {
-        audioService.skipToNext(path = path)
+    fun setDataSource(path: String) {
+        audioService.setDataSource(path = path)
     }
 }
