e2bc03baf2377d9ce594048686ed9a831f72d8f7
embarcados <gabriel.panza@aln.senaicimatec.edu.br>
2025-04-24 22:18:40 -0300

    Implementando lista de musica

--- playbackmodule/build.gradle.kts
+++ playbackmodule/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
